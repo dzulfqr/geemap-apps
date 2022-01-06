f834307a4b18496b92fa8a8aadc6fe96de3795e7
dzulfqr <54584410+dzulfqr@users.noreply.github.com>
2022-01-06 13:37:00 +0700

    Update marker.py

--- apps/marker.py
+++ apps/marker.py
@@ -13,16 +13,11 @@ def app():
 
     basemap = st.selectbox("Pilih Peta Dasar", keys)
     Map = geemap.Map()
-    file_path = os.path.abspath('../data/us-cities.json')
+    
+    url = 'https://github.com/giswqs/geemap/raw/master/examples/data/us-cities.json'
+    r = requests.get(url)
 
-    if not os.path.exists(file_path):
-        url = 'https://github.com/giswqs/geemap/raw/master/examples/data/us-cities.json'
-        r = requests.get(url)
-        with open(file_path, 'w') as f:
-            f.write(r.content.decode("utf-8"))        
-
-    with open(file_path) as f:
-        json_data = json.load(f)
+    json_data = json.loads(r.content.decode("utf-8"))
         
     maker_cluster = MarkerCluster(markers=[Marker(location=feature['geometry']['coordinates'][::-1]) for feature in json_data['features']], name = 'Markers')
     
